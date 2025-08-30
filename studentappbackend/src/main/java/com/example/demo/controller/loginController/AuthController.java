b6888372e2aa291fc3ad3925497bbe169aadfd39
Andrej659 <amarkanje@student.unizd.hr>
2025-08-30 04:52:33 +0200

    nez

--- studentappbackend/src/main/java/com/example/demo/controller/loginController/AuthController.java
+++ studentappbackend/src/main/java/com/example/demo/controller/loginController/AuthController.java
@@ -64,37 +64,3 @@ public class AuthController {
     }
 }
 
-class AuthRequest {
-    private String username;
-    private String password;
-
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-}
-
-class AuthResponse {
-    private String token;
-    private Integer role;
-
-    public AuthResponse(String token, Integer role) {
-        this.token = token;
-        this.role = role;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Integer getRole() {
-        return role;
-    }
-
-    public void setRole(Integer role) {
-        this.role = role;
-    }
-
-    public String getToken() { return token; }
-}
-
--- studentappbackend/src/main/java/com/example/demo/controller/loginController/AuthResponse.java
+++ studentappbackend/src/main/java/com/example/demo/controller/loginController/AuthResponse.java
@@ -1,4 +1,27 @@
 package com.example.demo.controller.loginController;
 
-public class AuthResponse {
+class AuthResponse {
+    private String token;
+    private Integer role;
+
+    public AuthResponse(String token, Integer role) {
+        this.token = token;
+        this.role = role;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Integer getRole() {
+        return role;
+    }
+
+    public void setRole(Integer role) {
+        this.role = role;
+    }
+
+    public String getToken() {
+        return token;
+    }
 }
